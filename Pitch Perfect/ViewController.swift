d3f8510f76350270a3d639382b20bfa1a316aaf2
joaobiriba <glaquidara@gmail.com>
2015-03-09 23:35:53 +0100

    First import

---
+++ Pitch Perfect/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Giovanni Biriba on 09/03/2015.
+//  Copyright (c) 2015 Laquysoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: AnyObject) {
+        //TODO: show test "recording in progress"
+        //TODO: Record user voice
+        println("in recordAudio");
+    }
+
+}
+
