3f775b35543f262910763c05958e90e25cb1ba02
joaobiriba <glaquidara@gmail.com>
2015-04-06 20:22:06 +0200

    stopAllAudio like the reviewer hints, label corrected

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -10,17 +10,15 @@ import UIKit
 import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-
+    
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
     var audioFile:AVAudioFile!
-
+    
     var audioEngine: AVAudioEngine!
-
+    
     @IBAction func playChipmunk(sender: UIButton) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopAllAudio()
         
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
@@ -36,36 +34,32 @@ class PlaySoundsViewController: UIViewController {
         audioEngine.startAndReturnError(nil)
         
         audioPlayerNode.play()
-
+        
     }
     
-
-    @IBAction func playEcho(sender: UIButton) {
     
-            audioPlayer.stop()
-            audioEngine.stop()
-            audioEngine.reset()
-            
-            var audioPlayerNode = AVAudioPlayerNode()
-            audioEngine.attachNode(audioPlayerNode)
-            
-            var changeEchoEffect = AVAudioUnitDelay()
-            changeEchoEffect.delayTime = 1
-            audioEngine.attachNode(changeEchoEffect)
-            
-            audioEngine.connect(audioPlayerNode, to: changeEchoEffect, format: nil)
-            audioEngine.connect(changeEchoEffect, to: audioEngine.outputNode, format: nil)
-            
-            audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
-            audioEngine.startAndReturnError(nil)
-            
-            audioPlayerNode.play()
+    @IBAction func playEcho(sender: UIButton) {
+        
+        stopAllAudio()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changeEchoEffect = AVAudioUnitDelay()
+        changeEchoEffect.delayTime = 1
+        audioEngine.attachNode(changeEchoEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changeEchoEffect, format: nil)
+        audioEngine.connect(changeEchoEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
     }
     
     @IBAction func playDarthvaderAudio(sender: UIButton) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopAllAudio()
         
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
@@ -81,46 +75,49 @@ class PlaySoundsViewController: UIViewController {
         audioEngine.startAndReturnError(nil)
         
         audioPlayerNode.play()
-
+        
     }
     @IBAction func slowSound(sender: UIButton) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopAllAudio()
         
         audioPlayer.rate = 0.5
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
-
-       
+        
+        
     }
-    @IBAction func stopSound(sender: UIButton) {
+    
+    func stopAllAudio() {
         audioPlayer.stop()
+        audioEngine.reset()
+        audioEngine.stop()
+    }
+    
+    @IBAction func stopSound(sender: UIButton) {
+        stopAllAudio()
     }
     @IBAction func speedSound(sender: UIButton) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopAllAudio()
         
         audioPlayer.rate = 2
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
     }
     override func viewDidLoad() {
-          audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
-
+        
         audioPlayer.enableRate = true
         audioEngine = AVAudioEngine()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    
+    
 }
